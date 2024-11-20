36c4205f03a7ded7b3d59b19154cfad630a9ccb7
News-s <84093019+news-s@users.noreply.github.com>
2024-11-20 21:35:40 +0100

    added ability to create a file with source code

--- zadaniator.py
+++ zadaniator.py
@@ -1,9 +1,35 @@
-from langchain_ollama import OllamaLLM
-from langchain.callbacks.manager import CallbackManager
-from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
-import config, time
+try:
+    from langchain_ollama import OllamaLLM
+    from langchain.callbacks.manager import CallbackManager
+    from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
+    import config, time
+except:
+    import os, sys
+    os.system("pip install requirements.txt")
+    print("Please run program agian")
+    sys.exit(1)
 
 llm = OllamaLLM(model=config.model, callback_manager=CallbackManager([StreamingStdOutCallbackHandler()]), temperature=config.temperature)
+
+def save_code_file(task_nr, content):
+    with open(f"zad{task_nr}_kod.html", 'w') as file:
+        file.write(f"""
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Kod</title>
+</head>
+<body>
+    <textarea style="width: 400px; height:850px;">
+        {content}
+    </textarea>
+</body>
+</html>
+
+""")
+        print("Zapisano")
 
 def save_to_file(task_nr, content):
     with open(f"zad{task_nr}.html", 'w') as file:
@@ -18,11 +44,15 @@ def main():
         if task.lower() == "exit":
             all_tasks_done = True
             break
-        zadanie = llm.invoke(config.template + task + "Pamiętaj o instrukcjach")
+        zadanie = llm.invoke(config.template + task + " Pamiętaj o instrukcjach")
         while True:
             if input("Czy odpowiedź spełnia wymagania? y/n: ") == 'y':
-                save_to_file(input("Podaj numer zadania: "), zadanie)
+                task_nr = input("Podaj numer zadania: ")
+                save_to_file(task_nr, zadanie)
+                if input("Czy trzeba wykonać plik z kodem źródłowym?: ").lower() == 'y':
+                    save_code_file(task_nr, zadanie)
                 llm.invoke("Zapomnij poprzednią konwersację. Odpowiedz tylko 'ok'")
+                print("\n")
                 czas = time.time() - start
                 print(f"Brawo, wykonałeś zadanie w {czas}, prawdopodobnie wykonałbyś to szybciej samemu leniwy debilu")
                 break
